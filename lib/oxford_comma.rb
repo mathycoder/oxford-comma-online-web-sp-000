8ed58b0a99059fad8f2169c1b54807cd298e5602
Adam S <adam.sarli@gmail.com>
2019-03-12 16:50:07 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,22 @@
 def oxford_comma(array)
-
+  if array.length == 1 
+    return array[0] 
+  elsif array.length == 2 
+    return array.join(" and ")
+  else
+    my_string = ""
+    index = 0 
+    while index < array.length - 1 
+      my_string << array[index] + ", "
+      index+=1 
+    end 
+    my_string << "and #{array[-1]}"
+    return my_string   
+  end 
 end
+
+
+puts oxford_comma(["kohlrabi"])
+puts oxford_comma(["fiddleheads","okra"])
+puts oxford_comma(["fiddleheads","okra","kohlrabi"])
+puts oxford_comma(["a","b","c","d","e"])
